453e73b6b5b9014f4827a9218f1b0c480f36ec89
Yazamoule <yazami.goto@gmail.com>
2025-02-12 13:24:17 +0100

    haaaaaaa

--- MalongoUnity/Assets/Scripts/TestTemp/TestForFmodOneShotSound.cs
+++ MalongoUnity/Assets/Scripts/TestTemp/TestForFmodOneShotSound.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
+using FMODUnity;
 
 public class TestForFmodOneShotSound : MonoBehaviour
 {
+
+    private StudioEventEmitter emitter;
+
+    LevelAudioEvents levelAudioEvents;
+
+    private void Start()
+    {
+        levelAudioEvents = LevelManager.Instance.auEvents;
+
+        emitter = GameManager.Instance.aum.InitializeEventEmitter(levelAudioEvents.coinIdle, this.gameObject);
+        emitter.Play();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        GameManager.Instance.aum.PlayOneShot(LevelManager.Instance.auEvents.coinCollected, this.transform.position);
+        GameManager.Instance.aum.PlayOneShot(levelAudioEvents.coinCollected, this.transform.position);
+    }
+
+    private void OnDestroy()
+    {
+        emitter.Stop();
     }
 }
